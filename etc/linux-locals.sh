22afa1a133f1bcfad219bf5e50e83d352af4e1b8
Changmin Choi <cmchoi9901@gmail.com>
2022-04-08 17:07:41 +0900

    add fzf install script

--- etc/linux-locals.sh
+++ etc/linux-locals.sh
@@ -452,6 +452,12 @@ install_pyenv() {
   git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
 }
 
+install_fzf() {
+    set -e
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+}
+
 # entrypoint script
 if [ `uname` != "Linux" ]; then
     echo "Run on Linux (not on Mac OS X)"; exit 1
