62f8ca8807c1f615a71d25c20d15e465c9a12ef6
Changmin Choi <cmchoi9901@gmail.com>
2023-08-23 18:18:03 +0900

    update scripts/install-neovim-py.sh

--- scripts/install-neovim-py.sh
+++ scripts/install-neovim-py.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # validate neovim package installation on python2/3 and automatically install if missing
 
+set -e
+
 RED="\033[0;31m";
 GREEN="\033[0;32m";
 YELLOW="\033[0;33m";
@@ -12,44 +14,14 @@ _version_check() {
     curver="${1/v/}"; targetver="$2";
     [ "$targetver" = "$(echo -e "$curver\n$targetver" | sort -V | head -n1)" ]
 }
-NVIM_MINIMUM_VERSION="0.5.1"
+NVIM_MINIMUM_VERSION="0.7.0"
+neovim_current_verson=$(nvim --version 2>/dev/null | head -n1 | cut -d' ' -f2)
 
-if which nvim >/dev/null && _version_check "$(nvim --version | head -n1 | cut -d' ' -f2)" "$NVIM_MINIMUM_VERSION"; then
-    echo -e "neovim found at ${GREEN}$(which nvim)${RESET}"
-    host_python3=""
-    [[ -z "$host_python3" ]] && host_python3="$(which python3)"
-    [[ -z "$host_python3" ]] && [[ -f "/usr/local/bin/python3" ]] && host_python3="/usr/local/bin/python3"
-    [[ -z "$host_python3" ]] && [[ -f "/usr/bin/python3" ]]       && host_python3="/usr/bin/python3"
-    if [[ -z "$host_python3" ]]; then
-        echo -e "${RED}  Python3 not found -- please have it installed in the system! ${RESET}";
-        exit 1;
-    fi
-    suggest_cmds=()
-    for py_bin in "$host_python3"; do
-        echo -e "Checking neovim package for the host python: ${GREEN}${py_bin}${RESET}"
-        neovim_ver=$($py_bin -c 'import pynvim; print("{major}.{minor}.{patch}".format(**pynvim.VERSION.__dict__))')
-        neovim_install_cmd="$py_bin -m pip install --user --upgrade pynvim"
-        rc=$?; if [[ $rc != 0 ]]; then
-            echo -e "${YELLOW}[!!!] Neovim requires 'pynvim' package on the host python. Try:${RESET}"
-            echo -e "${YELLOW}  $neovim_install_cmd${RESET}";
-            suggest_cmds+=("$neovim_install_cmd")
-        else  # check neovim is up-to-date
-            neovim_latest=$(python2 -c 'from xmlrpclib import ServerProxy; print(\
-                ServerProxy("http://pypi.python.org/pypi").package_releases("pynvim")[0])')
-            if [[ "$neovim_ver" != "$neovim_latest" ]]; then
-                echo -e "${YELLOW}  [!!] Neovim ($neovim_ver) is outdated (latest = $neovim_latest). Needs upgrade!${RESET}"
-                echo -e "${YELLOW}  $neovim_install_cmd${RESET}"
-                suggest_cmds+=("$neovim_install_cmd")
-            else
-                echo -e "${GREEN}  [OK] pynvim $neovim_ver${RESET}"
-            fi
-        fi
-    done
-    for cmd in "${suggest_cmds[@]}"; do
-        echo -e "\n${CYAN}Executing:${WHITE} $cmd ${RESET}"
-        $cmd;
-    done
+if which nvim >/dev/null && _version_check "$neovim_current_verson" "$NVIM_MINIMUM_VERSION"; then
+    echo -e "neovim found at ${GREEN}$(which nvim) ($neovim_current_verson)${RESET}"
+
 else
+    # neovim not found. install one!
     if [ `uname` == "Darwin" ]; then
         NEOVIM_INSTALL_CMD="brew install neovim"
     else
@@ -58,7 +30,7 @@ else
     if ! which nvim >/dev/null; then
         echo -e "${RED}Neovim not found."
     else
-        echo -e "${RED}Neovim is too old (recommended >= $NVIM_MINIMUM_VERSION): $(nvim --version | head -1)"
+        echo -e "${RED}Neovim is outdated (recommended >= $NVIM_MINIMUM_VERSION): $(nvim --version | head -1)"
     fi
     echo -e "Please install using '${NEOVIM_INSTALL_CMD}'.${RESET}"
 
@@ -66,8 +38,7 @@ else
     if [[ -n "$BASH_VERSION" ]] && [ `uname` == "Linux" ]; then
         while true; do
             echo -en "${YELLOW}Do you want to install neovim locally [y/N] ${RESET}"
-            [ -t 1 ] && read -t 5 -p "(wait 5 secs for auto-yes) ? " user_prompt;
-            if [ $? -ne 0 ]; then user_prompt="y"; fi  # when timeout, yes
+            [ -t 1 ] && read -t 5 -p "(wait 5 secs for auto-yes) ? " user_prompt || user_prompt="y";
             case $user_prompt in
                 [YyNn]* ) break;;
                 *) echo "Please answer yes or no.";;
@@ -81,3 +52,4 @@ else
 
     exit 1;
 fi
+
