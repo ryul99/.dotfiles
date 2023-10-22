9e6bddfedf1efc9315809a22d60e8a43609da154
Changmin Choi <cmchoi9901@gmail.com>
2023-10-22 14:35:05 +0900

    change cmp keymapping

--- home/.config/nvim/lua/config/cmp.lua
+++ home/.config/nvim/lua/config/cmp.lua
@@ -23,8 +23,8 @@ function M.setup()
       end,
     },
     mapping = {
-      ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-      ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+      ["<C-j>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+      ["<C-k>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
       ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
       ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
       ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
