faae74a13e2bf21fe1b20044a61fda097c2030f5
Changmin Choi <cmchoi9901@gmail.com>
2023-11-27 17:02:42 +0900

    update cmp.lua

--- home/.config/nvim/lua/config/cmp.lua
+++ home/.config/nvim/lua/config/cmp.lua
@@ -23,8 +23,8 @@ function M.setup()
       end,
     },
     mapping = {
-      ["<C-j>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-      ["<C-k>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+      ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+      ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
       ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
       ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
       ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
@@ -55,8 +55,6 @@ function M.setup()
       ["<S-Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
         else
           fallback()
         end
@@ -89,6 +87,16 @@ function M.setup()
       { name = "buffer" },
     },
   })
+
+  -- Use cmdline & path source for ':'
+  cmp.setup.cmdline(":", {
+    sources = cmp.config.sources({
+      { name = "path" },
+    }, {
+      { name = "cmdline" },
+    }),
+  })
 end
 
 return M
+
