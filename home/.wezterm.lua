19cc80399a75492f1834a701f7d4ee7594789d32
Changmin Choi <cmchoi9901@gmail.com>
2024-03-11 08:35:47 +0900

    add wezterm keymaps

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -1,5 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
+local keymaps = {}
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -12,6 +13,16 @@ color_scheme = 'dimidium'
 -- font
 config.font = wezterm.font('CaskaydiaMono Nerd Font Mono')
 config.font_size = 16
+
+-- keymaps Copy & Paste
+table.insert( keymaps, { key = 'C', mods = 'SHIFT|CTRL', action =
+wezterm.action.CopyTo 'ClipboardAndPrimarySelection', })
+
+table.insert( keymaps, { key = 'V', mods = 'SHIFT|CTRL', action =
+wezterm.action.PasteFrom 'Clipboard', })
+
+table.insert( keymaps, { key = 'V', mods = 'SHIFT|CTRL', action =
+wezterm.action.PasteFrom 'PrimarySelection', })
 
 -- and finally, return the configuration to wezterm
 return config
