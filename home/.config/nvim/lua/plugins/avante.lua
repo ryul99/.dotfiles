3b716d0fa35b18327c277623a2c235f9aa7cb2b0
Changmin Choi <cmchoi9901@gmail.com>
2024-09-04 00:23:54 +0900

    add avante provider: ollama, deepseek

--- home/.config/nvim/lua/plugins/avante.lua
+++ home/.config/nvim/lua/plugins/avante.lua
@@ -6,6 +6,62 @@ return {
         -- add any opts here
         provider = "openai",
         openai = { model = "gpt-4o-mini" },
+        vendors = {
+            ---@type AvanteProvider
+            ollama = {
+                ["local"] = true,
+                endpoint = "127.0.0.1:11434/v1",
+                model = "codegemma",
+                parse_curl_args = function(opts, code_opts)
+                    return {
+                        url = opts.endpoint .. "/chat/completions",
+                        headers = {
+                            ["Accept"] = "application/json",
+                            ["Content-Type"] = "application/json",
+                        },
+                        body = {
+                            model = opts.model,
+                            messages = require("avante.providers").copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
+                            max_tokens = 2048,
+                            stream = true,
+                        },
+                    }
+                end,
+                parse_response_data = function(data_stream, event_state, opts)
+                    require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+                end,
+            },
+
+            ---@type AvanteProvider
+            deepseek = {
+                endpoint = "https://api.deepseek.com/chat/completions",
+                model = "deepseek-coder",
+                api_key_name = "DEEPSEEK_API_KEY",
+                parse_curl_args = function(opts, code_opts)
+                    return {
+                        url = opts.endpoint,
+                        headers = {
+                            ["Accept"] = "application/json",
+                            ["Content-Type"] = "application/json",
+                            ["Authorization"] = "Bearer " .. os.getenv(opts.api_key_name),
+                        },
+                        body = {
+                            model = opts.model,
+                            messages = { -- you can make your own message, but this is very advanced
+                                { role = "system", content = code_opts.system_prompt },
+                                { role = "user",   content = require("avante.providers.openai").get_user_message(code_opts) },
+                            },
+                            temperature = 0,
+                            max_tokens = 4096,
+                            stream = true, -- this will be set by default.
+                        },
+                    }
+                end,
+                parse_response_data = function(data_stream, event_state, opts)
+                    require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+                end,
+            },
+        },
     },
     build = ":AvanteBuild", -- This is optional, recommended tho. Also note that this will block the startup for a bit since we are compiling bindings in Rust.
     dependencies = {
