863f01bae5adbdb4350bf99b23a52c8a42d5838d
Changmin Choi <cmchoi9901@gmail.com>
2024-12-06 04:02:25 +0900

    feat: use inherit in avante ollama

--- home/.config/nvim/lua/plugins/avante.lua
+++ home/.config/nvim/lua/plugins/avante.lua
@@ -11,27 +11,10 @@ return {
         vendors = {
             ---@type AvanteProvider
             ollama = {
+                __inherited_from = "openai",
                 api_key_name = '',
                 endpoint = "127.0.0.1:11434/v1",
                 model = "qwen2.5-coder",
-                parse_curl_args = function(opts, code_opts)
-                    return {
-                        url = opts.endpoint .. "/chat/completions",
-                        headers = {
-                            ["Accept"] = "application/json",
-                            ["Content-Type"] = "application/json",
-                        },
-                        body = {
-                            model = opts.model,
-                            messages = require("avante.providers").copilot.parse_messages(code_opts), -- you can make your own message, but this is very advanced
-                            max_tokens = 8192,
-                            stream = true,
-                        },
-                    }
-                end,
-                parse_response_data = function(data_stream, event_state, opts)
-                    require("avante.providers").openai.parse_response(data_stream, event_state, opts)
-                end,
             },
 
             ---@type AvanteProvider
