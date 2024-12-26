670dafce79949056c18282f38b6181e8375dce80
Changmin Choi <cmchoi9901@gmail.com>
2024-12-27 04:29:27 +0900

    feat(nvim): add which-key.nvim

--- home/config/nvim/lua/plugins/init.lua
+++ home/config/nvim/lua/plugins/init.lua
@@ -130,6 +130,24 @@ return {
             vim.o.foldenable = true
         end,
     },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {
+            delay = function(ctx)
+                return ctx.plugin and 0 or 1000
+            end,
+        },
+        keys = {
+            {
+                "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
+    },
 
     -- Language server and Auto completion
     "liuchengxu/vista.vim",
