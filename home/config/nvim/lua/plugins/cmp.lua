eabc5616e19a6c149b1f437bb72f2e472de08c65
Changmin Choi <cmchoi9901@gmail.com>
2025-02-27 14:06:00 +0900

    feat(copilot): enable for yaml, markdown

--- home/config/nvim/lua/plugins/cmp.lua
+++ home/config/nvim/lua/plugins/cmp.lua
@@ -149,6 +149,7 @@ return {
                     require("copilot").setup({
                         suggestion = { enabled = false },
                         panel = { enabled = false },
+                        filetypes = { yaml = true, markdown = true },
                     })
                 end,
             },
