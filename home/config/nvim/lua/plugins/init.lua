805ca29adc03d660a4646fa7b45220b8a9bdb8d5
Changmin Choi <cmchoi9901@gmail.com>
2025-04-23 13:39:20 +0900

    update copilot model to gpt-4o-copilot

--- home/config/nvim/lua/plugins/init.lua
+++ home/config/nvim/lua/plugins/init.lua
@@ -270,6 +270,7 @@ return {
                 suggestion = { enabled = false },
                 panel = { enabled = false },
                 filetypes = { yaml = true, markdown = true },
+                copilot_model = "gpt-4o-copilot",
             })
         end,
     },
