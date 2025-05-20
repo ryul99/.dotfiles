f44d8407e618ca43c386ff08a5faf84f18c4df0d
Changmin Choi <cmchoi9901@gmail.com>
2025-05-21 01:08:44 +0900

    add pyenv-update

--- scripts/linux-locals.sh
+++ scripts/linux-locals.sh
@@ -667,6 +667,7 @@ install_mujoco() {
 install_pyenv() {
   git clone https://github.com/pyenv/pyenv.git ~/.pyenv
   git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+  git clone https://github.com/pyenv/pyenv-update.git ~/.pyenv/plugins/pyenv-update
 }
 
 install_fzf() {
