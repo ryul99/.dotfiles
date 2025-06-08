e44d4f0060c815956ca8a551ae7fd28c6919d9ce
Changmin Choi <cmchoi9901@gmail.com>
2025-06-08 23:03:31 +0900

    feat(avante): add disable_tools

--- home/config/nvim/lua/plugins/avante.lua
+++ home/config/nvim/lua/plugins/avante.lua
@@ -1,3 +1,25 @@
+local disable_tools = {
+    "rag_search",
+    -- "python",
+    -- "git_diff",
+    "git_commit",
+    -- "list_files",
+    -- "search_files",
+    -- "search_keyword",
+    -- "read_file_toplevel_symbols",
+    -- "read_file",
+    -- "create_file",
+    -- "rename_file",
+    -- "delete_file",
+    -- "create_dir",
+    -- "rename_dir",
+    -- "delete_dir",
+    -- "bash",
+    "web_search",
+    "fetch",
+    "replace_in_file",
+}
+
 return {
     "yetone/avante.nvim",
     event = "VeryLazy",
@@ -11,17 +33,21 @@ return {
         providers = {
             openai = {
                 model = "gpt-4.1",
+                disable_tools = disable_tools,
             },
             gemini = {
                 model = "gemini-2.5-flash",
+                disable_tools = disable_tools,
                 extra_request_body = { max_tokens = 32768 },
             },
             copilot = {
                 model = "gpt-4.1",
+                disable_tools = disable_tools,
                 extra_request_body = { max_tokens = 32768 },
             },
             ollama = {
                 model = "gemma3:12b",
+                disable_tools = disable_tools,
                 endpoint = "http://127.0.0.1:11434",
                 extra_request_body = { max_tokens = 32768 },
             },
@@ -30,6 +56,7 @@ return {
                 api_key_name = '',
                 endpoint = "127.0.0.1:11433/v1",
                 model = "mlx-community/Qwen2.5-Coder-7B-Instruct-4bit",
+                disable_tools = disable_tools,
                 extra_request_body = { max_tokens = 16384 },
             },
         },
