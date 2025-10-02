2bf3f55fa93b0fffb282783603524e8a9c0c1e84
Changmin Choi <cmchoi9901@gmail.com>
2025-10-03 03:23:12 +0900

    remove windsurf.nvim

--- home/config/nvim/lua/plugins/init.lua
+++ home/config/nvim/lua/plugins/init.lua
@@ -336,15 +336,4 @@ return {
             })
         end,
     },
-    {
-        "Exafunction/windsurf.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "hrsh7th/nvim-cmp",
-        },
-        config = function()
-            require("codeium").setup({
-            })
-        end
-    },
 }
