44e8d5a0467fa24d550755796d38ad8941a9a42b
Changmin Choi <cmchoi9901@gmail.com>
2026-01-13 13:09:40 +0900

    fix telescope nvim to use latest

--- home/config/nvim/lua/plugins/init.lua
+++ home/config/nvim/lua/plugins/init.lua
@@ -66,7 +66,7 @@ return {
     },
     {
         "nvim-telescope/telescope.nvim",
-        version = "0.1.6",
+        version = "*",
         -- or                            , branch = '0.1.x',
         dependencies = { { "nvim-lua/plenary.nvim" } },
     },
