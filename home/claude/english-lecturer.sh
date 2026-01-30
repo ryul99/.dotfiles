ea4362511617f70d783b8842a8fc2e23d627b0a6
Changmin Choi <cmchoi9901@gmail.com>
2026-01-31 05:28:55 +0900

    change to english-lecturer.sh

---
+++ home/claude/english-lecturer.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+INPUT_PROMPT="$(cat | jq '.prompt')"
+
+if [[ -n "$REWRITER_LOCK" ]]; then
+    exit 0
+fi
+
+TARGET_LANGUAGE="Korean"
+JSON_SCHEMA='
+{
+    "type": "object",
+    "properties": {
+        "enhanced_prompt": { "type": "string" },
+        "lesson": { "type": "string" }
+    }
+}
+'
+
+INPUT_PROMPT="\
+Rewrite the following prompt in English to make it more grammatically correct and clear, while preserving its original meaning.
+If there are any mistakes, let me know with a brief explanation as a lesson in $TARGET_LANGUAGE.
+<PROMPT>
+$INPUT_PROMPT
+</PROMPT>\
+"
+
+RESPONSE="$( \
+    REWRITER_LOCK=1 claude \
+    --model sonnet \
+    --output-format json \
+    --json-schema "$JSON_SCHEMA" \
+    -p "$INPUT_PROMPT"
+)"
+
+STRUCTURED_OUTPUT="$(echo "$RESPONSE" | jq -r '.structured_output')"
+
+if [[ -z "$STRUCTURED_OUTPUT" ]]; then
+    exit 0
+fi
+
+OUTPUT_PROMPT="\
+This is lesson message for user. You should dismiss it.
+---
+$(echo "$STRUCTURED_OUTPUT" | jq -r .enhanced_prompt)
+---
+$(echo "$STRUCTURED_OUTPUT" | jq -r .lesson)
+"
+
+echo -e "$OUTPUT_PROMPT"
+exit 0
