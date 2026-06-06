27a69df7ca1185f30c21c43f88a1af7c4d249225
Changmin Choi <cmchoi9901@gmail.com>
2026-06-07 01:06:37 +0900

    nvim: fix treesitter

--- home/config/nvim/lua/plugins/treesitter.lua
+++ home/config/nvim/lua/plugins/treesitter.lua
@@ -22,17 +22,25 @@ return {
             vim.api.nvim_create_autocmd("FileType", {
                 group = vim.api.nvim_create_augroup("TreesitterHighlight", { clear = true }),
                 callback = function(args)
-                    local max_filesize = 1024 * 1024 -- 1MB
-                    local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(args.buf))
-
-                    if vim.bo[args.buf].filetype ~= "markdown" and (ok and stats and stats.size > max_filesize) then
+                    if vim.bo[args.buf].buftype ~= "" then
                         return
                     end
 
-                    local lang = vim.treesitter.language.get_lang(vim.bo[args.buf].filetype)
-                    if lang and pcall(vim.treesitter.get_parser, args.buf, lang) then
+                    local max_filesize = 1024 * 1024 -- 1MB
+                    local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(args.buf))
+
+                    local filetype = vim.bo[args.buf].filetype
+                    if filetype == "" then
+                        return
+                    end
+
+                    if filetype ~= "markdown" and (ok and stats and stats.size > max_filesize) then
+                        return
+                    end
+
+                    local lang = vim.treesitter.language.get_lang(filetype)
+                    if lang and pcall(vim.treesitter.start, args.buf, lang) then
                         -- vim.notify("Treesitter highlighting enabled for " .. lang, vim.log.levels.INFO)
-                        vim.treesitter.start(args.buf)
                         vim.bo[args.buf].syntax = 'ON'
                     end
                 end,
